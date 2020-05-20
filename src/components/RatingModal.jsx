c4d647ba669f6b397afe02d8ce1d314f5d82fd2d
Andy Cryan <acryan6@gatech.edu>
2020-05-20 08:53:27 -0600

    Add skeleton for modal

---
+++ src/components/RatingModal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RatingModal = ({ showModal }) => (
+  <div>{showModal === false ? null : <div>hi</div>}</div>
+);
+
+export default RatingModal;
--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,16 +1,34 @@
 import React from "react";
+import RatingModal from "./RatingModal.jsx";
 
-const StarRating = ({ main }) => (
-  <span>
-    <span class="solid-star"></span>
-    <span class="rating-box">
-      <span class="star-rating">
-        {main.avgreview ? main.avgreview.slice(0, 3) : null}
+class StarRating extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+    };
+  }
+
+  render() {
+    return (
+      <span>
+        <span class="solid-star"></span>
+        <span class="rating-box">
+          <span class="star-rating">
+            {this.props.main.avgreview
+              ? this.props.main.avgreview.slice(0, 3)
+              : null}
+          </span>
+          <span class="denominator">/10</span>
+          <div class="num-reviews quicklink">{this.props.main.numreviews}</div>
+        </span>
+
+        <span>
+          <RatingModal showModal={this.state.showModal} />
+        </span>
       </span>
-      <span class="denominator">/10</span>
-      <div class="num-reviews quicklink">{main.numreviews}</div>
-    </span>
-  </span>
-);
+    );
+  }
+}
 
 export default StarRating;
