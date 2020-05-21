fb6b2b6bbc8f8419297c3879f3140913cc858fb3
Andy Cryan <acryan6@gatech.edu>
2020-05-21 08:45:53 -0600

    Changes star color when hovering over rating star module

--- src/components/RatingModal.jsx
+++ src/components/RatingModal.jsx
@@ -6,11 +6,26 @@ class RatingModal extends React.Component {
     this.state = {
       hover: 0,
     };
-    this.handleHover = this.handleHover.bind(this);
+    // this.handleHover = this.handleHover.bind(this);
   }
 
   handleHover(e) {
-    console.log(e);
+    // console.log(e);
+    for (let i = 1; i <= Number(e); i++) {
+      document.getElementById(`star-${i}`).classList.add("rating-star-hover");
+    }
+    for (let j = Number(e) + 1; j <= 10; j++) {
+      console.log(document.getElementById(`star-${j}`).classList);
+      if (
+        document
+          .getElementById(`star-${j}`)
+          .classList.contains("rating-star-hover")
+      ) {
+        document
+          .getElementById(`star-${j}`)
+          .classList.remove("rating-star-hover");
+      }
+    }
   }
 
   render() {
@@ -20,44 +35,54 @@ class RatingModal extends React.Component {
       return (
         <div>
           <div
-            class="rating-star star-1"
-            onMouseEnter={this.handleHover("1")}
+            class="rating-star"
+            id="star-1"
+            onMouseEnter={this.handleHover.bind(this, "1")}
           ></div>
           <div
-            class="rating-star star-2"
-            onMouseEnter={this.handleHover("2")}
+            class="rating-star"
+            id="star-2"
+            onMouseEnter={this.handleHover.bind(this, "2")}
           ></div>
           <div
-            class="rating-star star-3"
-            onMouseEnter={this.handleHover("3")}
+            class="rating-star"
+            id="star-3"
+            onMouseEnter={this.handleHover.bind(this, "3")}
           ></div>
           <div
-            class="rating-star star-4"
-            onMouseEnter={this.handleHover("4")}
+            class="rating-star"
+            id="star-4"
+            onMouseEnter={this.handleHover.bind(this, "4")}
           ></div>
           <div
-            class="rating-star star-5"
-            onMouseEnter={this.handleHover("5")}
+            class="rating-star"
+            id="star-5"
+            onMouseEnter={this.handleHover.bind(this, "5")}
           ></div>
           <div
-            class="rating-star star-6"
-            onMouseEnter={this.handleHover("6")}
+            class="rating-star"
+            id="star-6"
+            onMouseEnter={this.handleHover.bind(this, "6")}
           ></div>
           <div
-            class="rating-star star-7"
-            onMouseEnter={this.handleHover("7")}
+            class="rating-star"
+            id="star-7"
+            onMouseEnter={this.handleHover.bind(this, "7")}
           ></div>
           <div
-            class="rating-star star-8"
-            onMouseEnter={this.handleHover("8")}
+            class="rating-star"
+            id="star-8"
+            onMouseEnter={this.handleHover.bind(this, "8")}
           ></div>
           <div
-            class="rating-star star-9"
-            onMouseEnter={this.handleHover("9")}
+            class="rating-star"
+            id="star-9"
+            onMouseEnter={this.handleHover.bind(this, "9")}
           ></div>
           <div
-            class="rating-star star-10"
-            onMouseEnter={this.handleHover("10")}
+            class="rating-star"
+            id="star-10"
+            onMouseEnter={this.handleHover.bind(this, "10")}
           ></div>
         </div>
       );
